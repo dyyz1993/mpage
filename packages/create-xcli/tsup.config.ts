78552050407a474f177265c2610ad38f68d751e5
xuyingzhou <xuyingzhou@tuzhanai.com>
2026-05-08 12:35:20 +0800

    fix(mpage): add shamefully-hoist in CI for workspace package resolution

--- packages/create-xcli/tsup.config.ts
+++ packages/create-xcli/tsup.config.ts
@@ -1,7 +1,4 @@
-import { resolve } from 'path';
 import { createConfig } from '../tsup.config.base';
-
-const coreDist = resolve(__dirname, '../core/dist/index.js');
 
 export default createConfig([
   {
@@ -10,7 +7,6 @@ export default createConfig([
     dts: false,
     clean: true,
     noExternal: ['@dyyz1993/xcli-core'],
-    alias: { '@dyyz1993/xcli-core': coreDist },
   },
   {
     entry: ['bin/create-xcli.ts'],
@@ -18,7 +14,6 @@ export default createConfig([
     dts: false,
     clean: false,
     noExternal: ['@dyyz1993/xcli-core'],
-    alias: { '@dyyz1993/xcli-core': coreDist },
     banner: {
       js: '#!/usr/bin/env node',
     },
